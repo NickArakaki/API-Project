3f2b6b89dc72025e337115f5f3fd3dd51d69bf5d
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-01-20 17:23:41 -0600

    Added User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -29,3 +29,12 @@ router.post(
         });
     }
 );
+
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+        res.clearCookie('token');
+        return res.json({ message: 'success' });
+    }
+);
