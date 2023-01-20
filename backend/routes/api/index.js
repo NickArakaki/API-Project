331ba464359bc4b26d2155667bc4f20f12ead439
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-01-20 17:59:05 -0600

    Add User signup backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { restoreUser } = require('../../utils/auth')
 const sessionRouter = require('./session');
-const userRouter = require('./users');
+const usersRouter = require('./users');
 
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the db
@@ -10,7 +10,7 @@ router.use(restoreUser);
 
 router.use('/session', sessionRouter);
 
-router.use('/user', userRouter);
+router.use('/users', usersRouter);
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
