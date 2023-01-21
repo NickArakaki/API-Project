8448a38798abec1fa4529950dfa475783122472a
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-01-20 18:23:06 -0600

    Add User input validation on user login backend endpoint

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -5,19 +5,19 @@ const handleValidationErrors = (req, _res, next) => {
     const validationErrors = validationResult(req);
 
     if (!validationErrors.isEmpty()) {
-        const errors = validationErrors
-            .array()
-            .map((error) => `${error.message}`);
+      const errors = validationErrors
+        .array()
+        .map((error) => `${error.msg}`);
 
-        const err = Error('Bad request.');
-        err.errors = errors;
-        err.title = 'Bad request.';
-        next(err);
+      const err = Error('Bad request.');
+      err.errors = errors;
+      err.status = 400;
+      err.title = 'Bad request.';
+      next(err);
     }
-
     next();
-};
+  };
 
-module.exports = {
+  module.exports = {
     handleValidationErrors
-};
+  };
