454a17d564aa8bc4a6f1f96d8926e7b9bf70c6cc
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-01-23 16:45:19 -0600

    Add associations for Booking to User and Spot

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -9,8 +9,22 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+
+/****************************** Associations **************************************/
     static associate(models) {
-      // define association here
+      // Booking -> User
+      Booking.belongsTo(
+        models.User,
+        {
+          foreignKey: 'userId'
+        });
+
+      // Booking -> Spot
+      Booking.belongsTo(
+        models.Spot,
+        {
+          foreignKey: 'spotId'
+        });
     }
   }
   Booking.init({
