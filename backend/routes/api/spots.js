21b370ff8e462853e15d60955c09f31975fec0ba
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-01-24 18:24:03 -0600

    add validator to spot route handler and update Spot model to reflect newly identified constraints

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,8 +1,54 @@
 const router = require('express').Router();
+const { appendFile } = require('fs');
 const { Spot, SpotImage, Review, sequelize } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+
+const validateAddSpot = [
+    check('address')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Street address is required'),
+    check('city')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('City is required'),
+    check('state')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('State is required'),
+    check('country')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Country is required'),
+    check('lat') // update once we know the specs
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Latitude not valid'),
+    check('lng') // update once we know the specs
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Longitude not valid'),
+    check('name') // update once we know the specs, update table and model
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .custom(val => val.length < 50)
+      .withMessage('Name must be less than 50 characters'),
+    check('description') // update once we know the specs, update table and model
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Description is required'),
+    check('price') // update once we know the specs, update table and model
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Price per day is required'),
+    handleValidationErrors
+  ];
+
 // GET all spots
-router.get('/', async (req, res, next) => {
+router.get('/', async (req, res) => {
     let spots = await Spot.findAll();
 
     spots = spots.map(spot => spot.toJSON());
@@ -46,6 +92,13 @@ router.get('/', async (req, res, next) => {
     }
 
     res.json({Spots: spots});
+});
+
+// POST a spot
+router.post('/', validateAddSpot, async (req, res, next) => {
+    const { address, city, state, ocuntry, lat, lng, name, description, price } = req.body;
+
+
 })
 
 module.exports = router;
