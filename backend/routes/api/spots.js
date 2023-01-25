c34576a3bde818453cad075642817e3a2ee2a58a
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-01-24 18:04:27 -0600

    add GET /api/spots/ endpoint to return all spots with averageRevew and previewImage

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,11 +1,51 @@
 const router = require('express').Router();
-const { Spot } = require('../../db/models');
+const { Spot, SpotImage, Review, sequelize } = require('../../db/models');
 
 // GET all spots
 router.get('/', async (req, res, next) => {
-    const spots = await Spot.findAll();
+    let spots = await Spot.findAll();
 
-    res.json({ Spots: spots });
+    spots = spots.map(spot => spot.toJSON());
+
+    for (let spot of spots) {
+        let avgRating = await Review.findOne({
+            attributes: {
+                include: [
+                    [
+                        sequelize.fn("AVG", sequelize.col("stars")),
+                        "avgRating"
+                    ]
+                ]
+            },
+            where: {
+                spotId : spot.id
+            }
+        });
+
+        if (avgRating) {
+            avgRating = avgRating.toJSON().avgRating;
+            spot.avgRating = avgRating;
+        } else {
+            spot.avgRating = null
+        }
+
+
+        let previewImage = await SpotImage.findOne({
+            where: {
+                spotId: spot.id,
+                preview: true
+            }
+        });
+
+        if (previewImage) {
+            previewImage = previewImage.toJSON().url;
+            spot.previewImage = previewImage;
+        } else {
+            spot.previewImage = 'No Preview Image Available'
+        }
+    }
+
+    res.json({Spots: spots});
 })
 
 module.exports = router;
