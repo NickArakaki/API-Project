fc64aebf7b2ae888ca638dfd1147f7fbb3f776ce
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-01-25 16:12:42 -0600

    Add PUT /api/reviews/:reviewId backend endpoint to allow authorized and authenticated user to update review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -5,6 +5,23 @@ const spotimage = require('../../db/models/spotimage');
 
 const { requireAuth } = require('../../utils/auth');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const validateReviewData = [
+    check('review')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Review text is required'),
+    check('stars')
+      .exists({ checkFalsy: true })
+      .isInt()
+      .withMessage('Stars must be an integer from 1 to 5'),
+    check('stars')
+      .custom( value => value > 5 || value < 1 ? false : true)
+      .withMessage('Stars must be an integer from 1 to 5'),
+      handleValidationErrors
+  ]
+
 // GET all Reviews of the current User
 router.get('/current', requireAuth, async (req, res, next) => {
     let reviews = await Review.findAll({
@@ -75,6 +92,30 @@ router.post('/:reviewId/images', requireAuth, async (req, res, next) => {
             id: reviewImage.id,
             url: reviewImage.url
         });
+    }
+})
+
+// UPDATE an existing review (REQ AUTHENTICATION AND AUTHORIZATION)
+    // validate review data
+router.put('/:reviewId', requireAuth, validateReviewData, async (req, res, next) => {
+    const userReview = await Review.findByPk(req.params.reviewId);
+
+    if (!userReview) {
+        const err = new Error("Review couldn't be found");
+        err.status = 404;
+        next(err);
+    } else if (userReview.userId !== req.user.id) {
+        const authorizationErr = new Error('Forbidden');
+        authorizationErr.status = 403;
+        next(authorizationErr);
+    } else {
+        const { review, stars } = req.body;
+
+        userReview.set({ review, stars });
+        userReview.validate();
+        await userReview.save();
+
+        res.json(userReview);
     }
 })
 
