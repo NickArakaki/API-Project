3110005b6016aac8d01bb9401be8620a489743c3
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-01-26 22:14:30 -0600

    Add constraints to query filter middleware to throw error if minimum values are greater than maximum values

--- backend/utils/query-filter.js
+++ backend/utils/query-filter.js
@@ -1,6 +1,10 @@
 const { Op } = require('sequelize');
 
 const queryFilter = (req, res, next) => {
+    const err = new Error('Invalid Search Filters')
+    err.status = 400;
+    err.errors = {}
+
     let size = req.query.size || 20;
     if (size <= 0 || size > 20) size = 20;
 
@@ -12,32 +16,39 @@ const queryFilter = (req, res, next) => {
 
     const minLat = req.query.minLat || -90;
     const maxLat = req.query.maxLat || 90;
+    if (minLat > maxLat) err.errors.lat = 'Minimum Latitude Must Be Less Than or Equal to Maximum Latitude';
 
     const minLng = req.query.minLng || -180;
     const maxLng = req.query.maxLng || 180;
+    if (minLng > maxLng) err.errors.lng = 'Minimum Longitude Must Be Less Than or Equal to Minimum Longitude';
 
     const minPrice = req.query.minPrice || 0;
     const maxPrice = req.query.maxPrice || 1000000; // who would pay more than 1,000,000 per day?
+    if (minPrice > maxPrice) err.errors.price = 'Minimum Price Must Be Less Than or Equal to Maximum Price'
 
-    const filter = {
-        where: {
-            lat: {
-                [Op.between]: [minLat, maxLat]
+    if (Object.keys(err.errors).length) {
+        next(err)
+    } else {
+        const filter = {
+            where: {
+                lat: {
+                    [Op.between]: [minLat, maxLat]
+                },
+                lng: {
+                    [Op.between]: [minLng, maxLng]
+                },
+                price: {
+                    [Op.between]: [minPrice, maxPrice]
+                }
             },
-            lng: {
-                [Op.between]: [minLng, maxLng]
-            },
-            price: {
-                [Op.between]: [minPrice, maxPrice]
-            }
-        },
-        limit,
-        offset
+            limit,
+            offset
+        }
+
+        req.queryFilter = filter;
+
+        next();
     }
-
-    req.queryFilter = filter;
-
-    next();
 }
 
 module.exports = {
