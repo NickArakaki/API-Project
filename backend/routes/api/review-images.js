93525e50b2f4f37fad398e8ccac96d3851979c89
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-01-29 21:02:00 -0600

    Refactor DELETE /api/review-images/:imageId to only allow authorized users to delete review images

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const { ReviewImage } = require('../../db/models');
+const { ReviewImage, Review } = require('../../db/models');
 
 /************************ Errors **********************/
 const { notFound , authorizationError } = require('../../utils/errors');
@@ -11,7 +11,9 @@ const { requireAuth } = require('../../utils/auth');
 /************************ Routes **********************/
 // DELETE Review Image (REQ AUTHENTICATION AND AUTHORIZATION)
 router.delete('/:imageId', requireAuth, async (req, res, next) => {
-    const reviewImage = await ReviewImage.findByPk(req.params.imageId);
+    const reviewImage = await ReviewImage.findByPk(req.params.imageId, {
+        include: [{ model: Review }]
+    });
 
     if (!reviewImage) {
         next(notFound('Review Image'))
