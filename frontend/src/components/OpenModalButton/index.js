b45c9e6f3137847b2dab09e74de7a79447e49158
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-02-12 21:45:20 -0600

    import css for open modal button in functional component

--- frontend/src/components/OpenModalButton/index.js
+++ frontend/src/components/OpenModalButton/index.js
@@ -1,5 +1,5 @@
 import { useModal } from "../../context/Modal";
-import './OpenModalButton.css';
+import "./OpenModalButton.css";
 
 function OpenModalButton({
   modalComponent, // component to render inside the modal
