b761b48552bb590576980650d2655a645d9201b4
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-02-13 22:44:02 -0600

    add more styling to landing page tiles

--- frontend/src/components/SpotTile/SpotTile.js
+++ frontend/src/components/SpotTile/SpotTile.js
@@ -10,9 +10,13 @@ export default function SpotTile({ spot }) {
     return (
         <div onClick={handleClick} className="spot_tile">
             <img className="spot_tile_img" src={spot.previewImage} alt={`${spot.name} preview image`}/>
-            <h3 className="spot_tile_spot_location">{spot.city}, {spot.state}</h3>
-            <div className="spot_tile_avg_stars"><i className="fa-solid fa-star" /> {spot.avgRating}</div>
-            <div className="spot_tile_price">${spot.price} night</div>
+            <div className="spot_tile_details_div">
+                <div className="spot_tile_location_and_price_div">
+                    <h3 className="spot_tile_spot_location">{spot.city}, {spot.state}</h3>
+                    <div className="spot_tile_price">${spot.price} night</div>
+                </div>
+                <div className="spot_tile_avg_stars"><i className="fa-solid fa-star" /> {spot.avgRating}</div>
+            </div>
         </div>
     )
 }
