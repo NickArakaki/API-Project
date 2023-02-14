1b57a4f3fb795ed8a412a4839b1a30164438916a
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-02-13 22:35:25 -0600

    updated landing page to display location instead of name for each spot

--- frontend/src/components/SpotTile/SpotTile.js
+++ frontend/src/components/SpotTile/SpotTile.js
@@ -10,7 +10,7 @@ export default function SpotTile({ spot }) {
     return (
         <div onClick={handleClick} className="spot_tile">
             <img className="spot_tile_img" src={spot.previewImage} alt={`${spot.name} preview image`}/>
-            <h3 className="spot_tile_spot_name">{spot.name}</h3>
+            <h3 className="spot_tile_spot_location">{spot.city}, {spot.state}</h3>
             <div className="spot_tile_avg_stars"><i className="fa-solid fa-star" /> {spot.avgRating}</div>
             <div className="spot_tile_price">${spot.price} night</div>
         </div>
