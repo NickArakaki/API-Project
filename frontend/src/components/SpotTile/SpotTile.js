dbf10db05b9d6cb33a0e05b3c5fe84bcd90aedf1
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-02-15 14:57:02 -0600

    add more styling to landing page

--- frontend/src/components/SpotTile/SpotTile.js
+++ frontend/src/components/SpotTile/SpotTile.js
@@ -4,7 +4,7 @@ export default function SpotTile({ spot }) {
             <img className="spot_tile_img" src={spot.previewImage} alt={`${spot.name} preview image`}/>
             <div className="spot_tile_details_div">
                 <div className="spot_tile_location_and_price_div">
-                    <h3 className="spot_tile_spot_location">{spot.city}, {spot.state}</h3>
+                    <div className="spot_tile_spot_location">{spot.city}, {spot.state}</div>
                     <div className="spot_tile_price">${spot.price} night</div>
                 </div>
                 <div className="spot_tile_avg_stars"><i className="fa-solid fa-star" /> {spot.avgRating ? Number(spot.avgRating).toFixed(1) : "new"}</div>
