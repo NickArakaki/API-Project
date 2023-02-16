6149f1e559e886a641e72d74bc2eaa509e76bcf9
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-02-15 18:12:05 -0600

    add styling for landing page tiles information (location, price)

--- frontend/src/components/SpotTile/SpotTile.js
+++ frontend/src/components/SpotTile/SpotTile.js
@@ -4,10 +4,14 @@ export default function SpotTile({ spot }) {
             <img className="spot_tile_img" src={spot.previewImage} alt={`${spot.name} preview image`}/>
             <div className="spot_tile_details_div">
                 <div className="spot_tile_location_and_price_div">
-                    <div className="spot_tile_spot_location">{spot.city}, {spot.state}</div>
-                    <div className="spot_tile_price">${spot.price} night</div>
+                    <div className="spot_tile_location_div">
+                        <div className="spot_tile_spot_city spot_tile_info">{spot.city}, </div>
+                        <div className="spot_tile_spot_state spot_tile_info">{spot.state}</div>
+                    </div>
+                    <br />
+                    <div className="spot_tile_price spot_tile_info">${spot.price} night</div>
                 </div>
-                <div className="spot_tile_avg_stars"><i className="fa-solid fa-star" /> {spot.avgRating ? Number(spot.avgRating).toFixed(1) : "new"}</div>
+                <div className="spot_tile_avg_stars spot_tile_info"><i className="fa-solid fa-star" /> {spot.avgRating ? Number(spot.avgRating).toFixed(1) : "new"}</div>
             </div>
         </>
     )
