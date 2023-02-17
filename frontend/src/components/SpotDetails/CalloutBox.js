b1e64508fc1374139a72c1e78e25fe16d8d8197c
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-02-17 15:58:08 -0600

    attempt to fix live render

--- frontend/src/components/SpotDetails/CalloutBox.js
+++ frontend/src/components/SpotDetails/CalloutBox.js
@@ -5,7 +5,7 @@ export default function CalloutBox({ spot }) {
         <>
             <div className="callout_box_price_reviews">
                 <div className="spot_details_callout_info_box_price">
-                    ${spot.price.toFixed(2)} <span className="spot_details_callout_info_box_night">night</span>
+                    ${Number(spot.price).toFixed(2)} <span className="spot_details_callout_info_box_night">night</span>
                 </div>
                 <div className="callout_box_reviews">
                     <ReviewsSummary spot={ spot } />
