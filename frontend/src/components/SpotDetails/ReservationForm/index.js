e2ae0990f5a87173df3f24913071294c118c408b
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-04-21 15:42:07 -0500

    date input moderately configured

--- frontend/src/components/SpotDetails/ReservationForm/index.js
+++ frontend/src/components/SpotDetails/ReservationForm/index.js
@@ -1,15 +1,37 @@
+import { useState } from 'react';
+import { DateRange } from 'react-date-range';
+// import {} from "../../../utils/reservationUtils"
+
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+
 function ReservationForm() {
+
+    const [dateRange, setDateRange] = useState([
+        {
+            startDate: new Date(),
+            endDate: new Date(),
+            key: 'selection'
+        }
+    ])
+
     const handleSubmit = (e) => {
         e.preventDefault();
         alert("this is the submit button working")
     }
+
     return (
         <form onSubmit={handleSubmit}>
-            <label>Start Date</label>
-            <input type="date"></input>
-            <label>End Date</label>
-            <input type="date"></input>
-            <button type="submit">Reserve</button>
+            <DateRange
+                onChange={item => setDateRange([item.selection])}
+                editableDateInputs={true}
+                moveRangeOnFirstSelection={false}
+                ranges={dateRange}
+                months={2}
+                direction="horizontal"
+                className="booking-input"
+            />
+            <button type='submit'>Reserve</button>
         </form>
     )
 }
