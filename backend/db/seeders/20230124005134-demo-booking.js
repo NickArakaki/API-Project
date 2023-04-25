bd929d3588c2e350643f0dee96d50c94033d1831
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-04-25 10:21:34 -0500

    add more booking seed data for demo user

--- backend/db/seeders/20230124005134-demo-booking.js
+++ backend/db/seeders/20230124005134-demo-booking.js
@@ -61,6 +61,30 @@ module.exports = {
         userId: 46,
         startDate: new Date('2021-03-27T00:00:00.000Z'),
         endDate: new Date('2021-03-30T00:00:00.000Z')
+      },
+      {
+        spotId: 3,
+        userId: 1,
+        startDate: new Date('2023-01-27T00:00:00.000Z'),
+        endDate: new Date('2023-02-09T00:00:00.000Z')
+      },
+      {
+        spotId: 4,
+        userId: 1,
+        startDate: new Date('2023-04-27T00:00:00.000Z'),
+        endDate: new Date('2023-04-30T00:00:00.000Z')
+      },
+      {
+        spotId: 5,
+        userId: 1,
+        startDate: new Date('2021-04-27T00:00:00.000Z'),
+        endDate: new Date('2021-04-30T00:00:00.000Z')
+      },
+      {
+        spotId: 6,
+        userId: 1,
+        startDate: new Date('2021-03-27T00:00:00.000Z'),
+        endDate: new Date('2021-03-30T00:00:00.000Z')
       }
     ]
     return await queryInterface.bulkInsert(options, bookings)
