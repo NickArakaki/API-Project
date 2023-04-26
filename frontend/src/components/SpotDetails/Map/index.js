4f7cfad5b11cceec3d2318f93a28852235ab794b
Nick Arakaki <nicholas.w.arakaki@gmail.com>
2023-04-26 13:08:59 -0500

    add map to spot details page

--- frontend/src/components/SpotDetails/Map/index.js
+++ frontend/src/components/SpotDetails/Map/index.js
@@ -1,10 +1,22 @@
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"
+
 import "./Map.css"
 
+
 function Map() {
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
+    })
+
+    const center = useMemo(() => ({ lat: 40, lng: -80}), [])
+
+    if (!isLoaded) return <div>Loading...</div>
+
     return (
-        <div className="map">
-            Hello From The Map Component
-        </div>
+        <GoogleMap zoom={10} center={center} mapContainerClassName="map">
+            <Marker position={center}></Marker>
+        </GoogleMap>
     )
 }
 
